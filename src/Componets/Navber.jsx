433efb37d7635ec2ad73bbfbdc0aae4993b4af4e
Md-Gajjali <hellogajjali@gmail.com>
2026-01-16 16:30:54 +0600

    update navber

--- src/Componets/Navber.jsx
+++ src/Componets/Navber.jsx
@@ -8,10 +8,76 @@ import { MdOutlineWork } from "react-icons/md";
 import { FaBook } from "react-icons/fa6";
 import { MdOutlineSms } from "react-icons/md";
 import ShinyText from '../Componets/ShinyText ';
+import { FloatingDock } from "../Componets/floating-dock";
+import {
+  IconBrandGithub,
+  IconBrandX,
+  IconExchange,
+  IconHome,
+  IconNewSection,
+  IconTerminal2,
+} from "@tabler/icons-react";
 
 
 
 const Navber = () => {
+      const links = [
+        {
+          title: "Home",
+          icon: (
+            <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+          ),
+          href: "#",
+        },
+    
+        {
+          title: "Products",
+          icon: (
+            <IconTerminal2 className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+          ),
+          href: "#",
+        },
+        {
+          title: "Components",
+          icon: (
+            <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+          ),
+          href: "#",
+        },
+        {
+          title: "Aceternity UI",
+          icon: (
+            <img
+              src="https://assets.aceternity.com/logo-dark.png"
+              width={20}
+              height={20}
+              alt="Aceternity Logo" />
+          ),
+          href: "#",
+        },
+        {
+          title: "Changelog",
+          icon: (
+            <IconExchange className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+          ),
+          href: "#",
+        },
+    
+        {
+          title: "Twitter",
+          icon: (
+            <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+          ),
+          href: "#",
+        },
+        {
+          title: "GitHub",
+          icon: (
+            <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+          ),
+          href: "#",
+        },
+      ];
   return (
     <>
         <nav className=' '>
@@ -22,7 +88,7 @@ const Navber = () => {
                         text="✨Emam Gajjali"
                         speed={2}
                         delay={0}
-                        color="#b5b5b5"
+                        color="#A395E9"
                         shineColor="#ffffff"
                         spread={120}
                         direction="left"
@@ -37,6 +103,12 @@ const Navber = () => {
                         <li><MdOutlineWork /></li>
                         <li><FaBook /></li>
                         <li><MdOutlineSms /></li>
+                        <div className="flex items-center justify-center h-[35rem] w-full">
+                              <FloatingDock
+                                // only for demo, remove for production
+                                mobileClassName="translate-y-20"
+                                items={links} />
+                        </div>
                     </ul>
                     <div>
                         <FaMoon />
