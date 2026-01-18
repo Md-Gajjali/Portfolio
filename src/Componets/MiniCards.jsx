8265dd7d699fb394d647b2b00f839a61f138ef59
Md-Gajjali <hellogajjali@gmail.com>
2026-01-18 13:34:00 +0600

    update button

--- src/Componets/MiniCards.jsx
+++ src/Componets/MiniCards.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MiniCards = ({heading,title,icons}) => {
   return (
-    <div className='w-50 h-full p-2.5 g-white -bg-opacity-10 bg-white backdrop-blur-md rounded-lg border border-white border-opacity-30">;'>
+    <div className='w-50 h-full p-2.5 g-white bg-opacity-10 bg-white backdrop-blur-md rounded-lg border border-white border-opacity-30">;'>
       <p className='text-primariy text-[20px]' >{icons}</p>
       <h3 className='text-white opacity-85 text-2xl '>{heading}</h3>
       <p className=' text-secondary '>{title}</p>
